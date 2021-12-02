97a2dacbfa78ca5429d4ccfa79fb6a3370d5cdf1
Steven Goff <steveng9@gmail.com>
2021-12-01 23:20:22 -0600

    Refactor 2021 day 2

--- src/2021/02/index.ts
+++ src/2021/02/index.ts
@@ -4,28 +4,30 @@ import assert from 'assert';
 const rawInput = readInput();
 const input = rawInput.split('\n').map(String);
 
+/* Types */
+type Command = {
+  forward: (qty: number) => void;
+  up: (qty: number) => void;
+  down: (qty: number) => void;
+};
+
 /* Functions */
+const processCommand = (values: string[], commands: Command) => {
+  values.forEach((value) => {
+    const [command, qtyString] = value.split(' ');
+    const qty = parseInt(qtyString, 10);
+    commands[command]?.(qty);
+  });
+};
 
 function part1(values: string[]): number {
   let horizontal = 0;
   let depth = 0;
 
-  values.forEach((value) => {
-    const [command, qtyString] = value.split(' ');
-    const qty = parseInt(qtyString, 10);
-    switch (command) {
-      case 'forward':
-        horizontal += qty;
-        break;
-      case 'up':
-        depth -= qty;
-        break;
-      case 'down':
-        depth += qty;
-        break;
-      default:
-        break;
-    }
+  processCommand(values, {
+    forward: (qty) => (horizontal += qty),
+    up: (qty) => (depth -= qty),
+    down: (qty) => (depth += qty),
   });
   return horizontal * depth;
 }
@@ -35,44 +37,16 @@ function part2(values: string[]): number {
   let depth = 0;
   let aim = 0;
 
-  //   const commands = {
-  //     forward: (qty) => {
-  //       horizontal += qty;
-  //       depth += aim * qty;
-  //     },
-  //     up: (qty) => (aim -= qty),
-  //     down: (qty) => (aim += qty),
-  //   };
-
-  values.forEach((value) => {
-    // console.log('V: ', value);
-    const [command, qtyString] = value.split(' ');
-    const qty = parseInt(qtyString, 10);
-    // commands[command]?.(qty);
-    switch (command) {
-      case 'forward':
-        horizontal += qty;
-        depth += aim * qty;
-        break;
-      case 'up':
-        // depth -= qty;
-        aim -= qty;
-        break;
-      case 'down':
-        // depth += qty;
-        aim += qty;
-        break;
-      default:
-        break;
-    }
+  processCommand(values, {
+    forward: (qty) => {
+      horizontal += qty;
+      depth += aim * qty;
+    },
+    up: (qty) => (aim -= qty),
+    down: (qty) => (aim += qty),
   });
   return horizontal * depth;
 }
-
-/* Tests */
-
-assert.strictEqual(part1(['forward 5', 'down 5', 'forward 8', 'up 3', 'down 8', 'forward 2']), 150);
-assert.strictEqual(part1(input), 1693300);
 
 assert.strictEqual(part2(['forward 5', 'down 5', 'forward 8', 'up 3', 'down 8', 'forward 2']), 900);
 assert.strictEqual(part2(input), 1857958050);
