2d1ca5c29fb179c07fbe338d086101f04b5f5e87
Steven Goff <steveng9@gmail.com>
2021-12-03 00:21:56 -0600

    Refactor

--- src/2021/03/index.ts
+++ src/2021/03/index.ts
@@ -4,35 +4,14 @@ import assert from 'assert';
 const rawInput = readInput();
 const input = rawInput.split('\n').map(String);
 
-/* Functions */
-
-function part1(values: string[]): number {
-  let gamma = '';
-  let epsilon = '';
-  for (let i = 0; i < values[0].length; i++) {
-    let sum = 0;
-    for (let j = 0; j < values.length; j++) {
-      sum += parseInt(values[j][i], 10) === 1 ? 1 : -1;
-    }
-    // console.log('Sum: ', sum);
-    // const isOne = sum / 2 > values[0].length / 2;
-    const isOne = sum >= 0;
-    gamma += isOne ? '1' : '0';
-    epsilon += isOne ? '0' : '1';
-  }
-
-  console.log('Gamma: ', gamma);
-  console.log('Epsilon: ', epsilon);
-  return parseInt(gamma, 2) * parseInt(epsilon, 2);
-}
-
+/* Binary Tree */
 export class TreeNode<T> {
-  key: T;
+  key: string;
   quantity: number;
   left: TreeNode<T>;
   right: TreeNode<T>;
 
-  constructor(val: T) {
+  constructor(val: string) {
     this.key = val;
     this.quantity = 0;
     this.left = null;
@@ -44,82 +23,92 @@ export class TreeNode<T> {
   }
 
   depth() {
-    // return 1 + Math.max(0, this.left?.depth(), this.right?.depth());
     const leftDepth = this.left?.depth() ?? 0;
     const rightDepth = this.right?.depth() ?? 0;
     return 1 + Math.max(0, leftDepth, rightDepth);
   }
+
+  addChild(val: string) {
+    if (val === '1') {
+      if (this.right == null) this.right = new TreeNode(val);
+      this.right.quantity += 1;
+      return this.right;
+    } else {
+      if (this.left == null) this.left = new TreeNode(val);
+      this.left.quantity += 1;
+      return this.left;
+    }
+  }
+
+  /**
+   * Ties to the right
+   * @returns
+   */
+  getPathMostTravelled<T>() {
+    const oneMoreFrequent = this.right?.quantity >= this.left?.quantity;
+    const oneDeepOrDeeper = this.right?.depth() >= this.left?.depth();
+
+    const oneWins = (oneMoreFrequent && oneDeepOrDeeper) || this.left == null;
+    const selfKey = this.key ?? '';
+    const childKey = (oneWins ? this.right?.getPathMostTravelled() : this.left?.getPathMostTravelled()) ?? '';
+    return selfKey + childKey;
+  }
+  /**
+   * Ties to the left
+   * @returns
+   */
+  getPathLeastTravelled<T>() {
+    const zeroLessFrequent = this.left?.quantity <= this.right?.quantity;
+    const zeroAsDeep = this.left?.depth() >= this.right?.depth();
+
+    const zeroWins = (zeroLessFrequent && zeroAsDeep) || this.right == null;
+    const selfKey = this.key ?? '';
+    const childKey = (zeroWins ? this.left?.getPathLeastTravelled() : this.right?.getPathLeastTravelled()) ?? '';
+    return selfKey + childKey;
+  }
 }
 
-function part2(values: string[]): number {
-  const root = new TreeNode(undefined);
+/**
+ * Builds binary tree, root node is a placeholder, left is always 0 and right 1 (when not null)
+ * @param values
+ * @returns
+ */
+function buildTree(values: string[]) {
+  let root = new TreeNode(undefined);
   values.forEach((value) => {
     let previousNode = root;
     value.split('').forEach((char, idx) => {
-      let node = char === '1' ? previousNode.right : previousNode.left;
-      if (node === null) {
-        node = new TreeNode(char);
-        if (char === '1') {
-          previousNode.right = node;
-        } else {
-          previousNode.left = node;
-        }
-      }
-      node.quantity += 1;
-      previousNode = node;
+      previousNode = previousNode.addChild(char);
     });
   });
+  return root;
+}
 
-  // Traverse tree
+/* Functions */
 
-  // o2 rating = find most common, favoring 1 in tie
-  let o2Rating = '';
-  let currentNode = root;
-  while (currentNode != null && currentNode.hasChild()) {
-    const oneWins = currentNode.right?.quantity >= currentNode.left?.quantity || currentNode.left == null;
-    if (oneWins) {
-      o2Rating += currentNode.right?.key;
-      currentNode = currentNode.right;
-    } else {
-      o2Rating += currentNode.left?.key;
-      currentNode = currentNode.left;
+function part1(values: string[]): number {
+  let gamma = '';
+  let epsilon = '';
+  for (let i = 0; i < values[0].length; i++) {
+    let sum = 0;
+    for (let j = 0; j < values.length; j++) {
+      sum += parseInt(values[j][i], 10) === 1 ? 1 : -1;
     }
-  }
-  console.log('o2: ', o2Rating);
-
-  // co2 scrubber rating = find least common, favoring 0 in tie
-  let co2Rating = '';
-  currentNode = root;
-  console.log('Root depth: ', currentNode.depth());
-  while (currentNode != null && currentNode.hasChild()) {
-    console.log(
-      `(0 qty: ${currentNode.left?.quantity} depth ${currentNode.left?.depth()}) vs (1 qty ${
-        currentNode.right?.quantity
-      } depth ${currentNode.right?.depth()})`,
-    );
-    const zeroWins =
-      (currentNode.left?.quantity <= currentNode.right?.quantity &&
-        currentNode.left?.depth() >= currentNode.right?.depth()) ||
-      currentNode.right == null;
-    // const oneWins = currentNode.right?.quantity >= currentNode.left?.quantity && currentNode.right.hasChild();
-    if (zeroWins) {
-      console.log('0 wins with depth ' + currentNode.depth());
-      co2Rating += currentNode.left?.key;
-      currentNode = currentNode.left;
-    } else {
-      console.log('1 wins with depth ' + currentNode.depth());
-      co2Rating += currentNode.right?.key;
-      currentNode = currentNode.right;
-    }
+    const isOne = sum > 0;
+    gamma += isOne ? '1' : '0';
+    epsilon += isOne ? '0' : '1';
   }
 
-  // return o2 * co2
+  return parseInt(gamma, 2) * parseInt(epsilon, 2);
+}
 
-  //   console.log('Root: ', root);
-  console.log('co2: ', co2Rating);
-  // Build tree?
+function part2(values: string[]): number {
+  let root = buildTree(values);
+
+  let o2Rating = root.getPathMostTravelled();
+  let co2Rating = root.getPathLeastTravelled();
+
   return parseInt(o2Rating, 2) * parseInt(co2Rating, 2);
-  //   return 0;
 }
 
 /* Tests */
