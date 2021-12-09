aa4e20586091813dcacc3a6597592289b95d0a62
Steven Goff <steveng9@gmail.com>
2021-12-09 01:02:57 -0600

    Complete day 9 refactor

--- src/2021/09/index.ts
+++ src/2021/09/index.ts
@@ -24,14 +24,14 @@ function getBasinPositions(
   inBasin: Set<string> = new Set(),
   visited: Set<string> = new Set(),
 ) {
-  if (grid.getVector(position) === undefined || grid.getVector(position) === 9 || visited.has(position.toString())) {
-    visited.add(position.toString());
+  const positionForSet = position.toString(); // use string set requires JS primitives
+  if (grid.getVector(position) === undefined || grid.getVector(position) === 9 || visited.has(positionForSet)) {
+    visited.add(positionForSet);
     return { visited, inBasin };
   } else {
-    visited.add(position.toString());
-    inBasin.add(position.toString());
-    const neighbors = grid.getNeighborPositions(position);
-    neighbors.forEach((neighbor) => {
+    visited.add(positionForSet);
+    inBasin.add(positionForSet);
+    grid.getNeighborPositions(position).forEach((neighbor) => {
       const results = getBasinPositions(grid, neighbor, inBasin, visited);
       visited = results.visited;
       inBasin = results.inBasin;
@@ -39,62 +39,20 @@ function getBasinPositions(
     return { visited, inBasin };
   }
 }
-// function getBasinPositions(
-//   grid: Grid<number>,
-//   position: Vector2,
-//   inBasin: Set<string> = new Set(),
-//   visited: Set<string> = new Set(),
-// ) {
-//   if (visited.has(position.toString())) {
-//     return { inBasin, visited };
-//   }
-//   inBasin.add(position.toString());
-//   visited.add(position.toString());
-//   const neighbors = grid.getNeighborPositions(position);
-//   for (let neighbor of neighbors) {
-//     const neighborValue = grid.getVector(neighbor);
-//     if (neighborValue !== undefined && neighborValue !== 9 && !visited.has(neighbor.toString())) {
-//       const results = getBasinPositions(grid, neighbor, inBasin, visited);
-//       visited = results.visited;
-//       inBasin = results.inBasin;
-//     } else {
-//       visited.add(neighbor.toString());
-//     }
-//   }
-//   return { inBasin, visited };
-// }
 
 function part2(values: string[]): number {
   const grid = parse(values);
 
-  // get low points
   const lowPoints: Vector2[] = [];
-
   grid.forEach((pos, val) => {
     if (val < Math.min(...grid.getNeighborValues(pos))) {
       lowPoints.push(pos);
     }
   });
-  // grid.map((pos, val) => {
-  //   // console.log(`Pos:, ${pos}, val: ${val}`);
-  //   if (val < Math.min(...grid.getNeighborValues(pos))) {
-  //     // console.log(`Low point at pos ${pos} with val ${val} ${grid.getNeighbors(pos)}`);
-  //     // retVal += val + 1;
-  //     lowPoints.push(pos);
-  //   }
-  //   return val;
-  // });
 
-  const basinSizes = lowPoints
-    .map((position) => {
-      // console.log(`===Get basin pos for ${position}`);
-      const positions = getBasinPositions(grid, position);
-      // console.log('Basin: ', positions.inBasin.size);
-      return positions.inBasin.size;
-    })
-    .sort((a, b) => b - a);
-  const threeLargest = basinSizes.slice(0, 3);
-  return threeLargest.reduce((a, b) => a * b);
+  const basinSizes = lowPoints.map((position) => getBasinPositions(grid, position).inBasin.size);
+  const threeLargestBasins = basinSizes.sort((a, b) => b - a).slice(0, 3);
+  return threeLargestBasins.reduce((a, b) => a * b);
 }
 
 /* Tests */
