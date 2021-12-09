38b321499b76d4c209974f22c2902264297ebbc7
Steven Goff <steveng9@gmail.com>
2021-12-08 23:03:45 -0600

    Stub out 2021 09

---
+++ src/2021/09/index.ts
@@ -0,0 +1,31 @@
+import readInput from '../../utils/readInput';
+import assert from 'assert';
+
+const rawInput = readInput();
+const input = rawInput.split('\n').map(Number);
+
+/* Functions */
+
+function part1(values: number[]): number {
+return 0;
+}
+
+function part2(values: number[]): number {
+return 0;
+}
+
+/* Tests */
+
+assert.strictEqual(part1([1, 1, 1]), 0);
+
+assert.strictEqual(part2([1, -1]), 0);
+
+/* Results */
+
+console.time('Time');
+// const resultPart1 = part1(input);
+// const resultPart2 = part2(input);
+console.timeEnd('Time');
+
+// console.log('Solution to part 1:', resultPart1);
+// console.log('Solution to part 2:', resultPart2);
