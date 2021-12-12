d4204b06e3c853e7fe8321b1a239a699ec472681
Steven Goff <steveng9@gmail.com>
2021-12-12 13:29:06 -0600

    2021 12 pt 2 solution

--- src/2021/12/index.ts
+++ src/2021/12/index.ts
@@ -1,7 +1,7 @@
 import readInput, { readDemoInput } from '../../utils/readInput';
 import assert from 'assert';
 import { Graph, Node, Queue } from '../../utils/graph';
-import { tail } from '../../utils/array';
+import { countItems, findDuplicates, tail } from '../../utils/array';
 
 const rawInput = readInput();
 const input = rawInput.split('\n');
@@ -45,17 +45,58 @@ function findPathsToEnd(graph: Graph<string>) {
   return finalizedPaths;
 }
 
+function isInvalidCaveRentry(node: Node<string>, path: Node<string>[]) {
+  const isStart = node.data === 'start';
+  const isLowerCase = node.data == node.data.toLowerCase();
+  if (isStart) {
+    return true;
+  } else if (isLowerCase) {
+    const onlyLowerCase = path.map((i) => i.data).filter((i) => i === i.toLowerCase());
+    const existingDuplicates = findDuplicates(countItems(onlyLowerCase));
+    // Allow another instance of lowercase as long as there are no other dupicates
+    return path.includes(node) && existingDuplicates.length > 0;
+  }
+  return false;
+}
+
+function findPathsToEndPt2(graph: Graph<string>) {
+  const startNode = graph.nodes.get('start');
+  const endNode = graph.nodes.get('end');
+
+  const pendingPaths: Queue<Node<string>[]> = new Queue();
+  const finalizedPaths: Node<string>[][] = [];
+  pendingPaths.add([startNode]);
+
+  while (!pendingPaths.isEmpty()) {
+    const path = pendingPaths.remove();
+    tail(path).adjacent.forEach((node: Node<string>) => {
+      if (node === endNode) {
+        finalizedPaths.push([...path, node]);
+      } else if (isInvalidCaveRentry(node, path)) {
+        // don't repeat lowercase nodes
+      } else {
+        pendingPaths.add([...path, node]);
+      }
+    });
+  }
+  return finalizedPaths;
+}
+
 function part1(values: string[]): number {
   const graph = parse(values);
   const validPaths = findPathsToEnd(graph);
   return validPaths.length;
 }
 
-function part2(values: number[]): number {
-  return 0;
+function part2(values: string[]): number {
+  const graph = parse(values);
+  const validPaths = findPathsToEndPt2(graph);
+  //   console.log(validPaths.map((i) => i.map((j) => j.data)));
+  return validPaths.length;
 }
 
 /* Tests */
+assert.strictEqual(part2(demoInput), 36);
 
 assert.strictEqual(part1(demoInput), 10);
 assert.strictEqual(
@@ -64,14 +105,18 @@ assert.strictEqual(
 );
 assert.strictEqual(part1(input), 5756);
 
-assert.strictEqual(part2([1, -1]), 0);
+assert.deepStrictEqual(countItems(['a', 'b', 'c', 'a']), { a: 2, b: 1, c: 1 });
+assert.deepStrictEqual(findDuplicates(countItems(['a', 'b', 'c', 'a'])), ['a']);
+assert.deepStrictEqual(findDuplicates(countItems(['a', 'b', 'c'])), []);
 
 /* Results */
 
 console.time('Time');
 const resultPart1 = part1(input);
-// const resultPart2 = part2(input);
 console.timeEnd('Time');
-
 console.log('Solution to part 1:', resultPart1);
-// console.log('Solution to part 2:', resultPart2);
+
+console.time('Time');
+const resultPart2 = part2(input);
+console.timeEnd('Time');
+console.log('Solution to part 2:', resultPart2); // Not 39461
--- src/utils/array.ts
+++ src/utils/array.ts
@@ -3,3 +3,7 @@ export const arrayToSingleNumber = (array: number[]) => Number(array.join(''));
 export const tail = (array) => array[array.length - 1];
 
 export const sum = (acc: number, i: number) => acc + i;
+
+export const countItems = (names) => names.reduce((a, b) => ({ ...a, [b]: (a[b] || 0) + 1 }), {});
+
+export const findDuplicates = (dict) => Object.keys(dict).filter((a) => dict[a] > 1);
