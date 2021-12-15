5580938db16e4f534bff7d09e456072954a021f3
Steven Goff <steveng9@gmail.com>
2021-12-14 23:38:59 -0600

    2021 15 refactor

--- src/2021/15/index.ts
+++ src/2021/15/index.ts
@@ -4,23 +4,41 @@ import { Graph, astar } from '../../utils/astar';
 
 const rawInput = readInput();
 
-const parse = (input) => input.split('\n').map((line) => line.split('').map(Number));
+const parse = (input: string) => input.split('\n').map((line) => line.split('').map(Number));
 
 /* Functions */
 
-function part1(values: number[][]): number {
+function findLeastRiskyPathWeight(values: number[][]): number {
   const graph = new Graph(values);
   const start = graph.grid[0][0];
   const end = graph.grid[graph.grid.length - 1][graph.grid[0].length - 1];
   const result = astar.search(graph, start, end);
 
-  const risk = result.reduce((sum, i) => sum + i.weight, 0);
-  return risk;
+  return result.reduce((sum: number, i) => sum + i.weight, 0);
 }
 
-function part2(values: number[]): number {
-  return 0;
-}
+const getHigherLevelRisk = (max: number, base: number, increment: number) => ((base - 1 + increment) % max) + 1;
+
+const getGrid5x = (values: number[][]): number[][] => {
+  const lengthX = values[0].length;
+  const lengthY = values.length;
+
+  let retVal = [];
+  for (let i = 0; i < lengthY * 5; i++) {
+    retVal[i] = [];
+  }
+
+  for (let y = 0; y < lengthY; y++) {
+    for (let x = 0; x < lengthX; x++) {
+      for (let yMod = 0; yMod < 5; yMod++) {
+        for (let xMod = 0; xMod < 5; xMod++) {
+          retVal[y + yMod * lengthY][x + xMod * lengthX] = getHigherLevelRisk(9, values[y][x], yMod + xMod);
+        }
+      }
+    }
+  }
+  return retVal;
+};
 
 /* Tests */
 
@@ -35,16 +53,17 @@ const demo = `1163751742
 1293138521
 2311944581`;
 
-assert.strictEqual(part1(parse(demo)), 40);
+assert.strictEqual(findLeastRiskyPathWeight(parse(demo)), 40);
+assert.strictEqual(findLeastRiskyPathWeight(getGrid5x(parse(demo))), 315);
 
-// assert.strictEqual(part2([1, -1]), 0);
-
+assert.strictEqual(findLeastRiskyPathWeight(parse(rawInput)), 755);
+assert.strictEqual(findLeastRiskyPathWeight(getGrid5x(parse(rawInput))), 3016);
 /* Results */
 
 console.time('Time');
-const resultPart1 = part1(parse(rawInput));
-// const resultPart2 = part2(input);
+const resultPart1 = findLeastRiskyPathWeight(parse(rawInput));
+const resultPart2 = findLeastRiskyPathWeight(getGrid5x(parse(rawInput)));
 console.timeEnd('Time');
 
 console.log('Solution to part 1:', resultPart1);
-// console.log('Solution to part 2:', resultPart2);
+console.log('Solution to part 2:', resultPart2);
